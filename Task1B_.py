0434fe1c8946baf340a882979440fb85cf670c65
rg638 <rg638@cam.ac.uk>
2021-01-19 20:50:47 +0000

    Trying to fix file issue, solved by renaming Task1B to Task1B_ and chaning the reference in .github/workflows/pythonapp.yml

--- Task1B_.py
+++ Task1B_.py
@@ -2,6 +2,9 @@ from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
 
+#  This is named Task1B_ rather than Task1B as pycharm was not detecting my file as a python file with the old name
+#  Have changed the reference for the file in .github/workflows/pythonapp.yml to Task1B_ to account for this
+
 def run():
     """Requirements for Task 1B"""
 
