c458f3f7700a945c4ca9074d1cadcd9707347040
rg638 <rg638@cam.ac.uk>
2021-01-19 16:04:25 +0000

    Implemented function, now need to create demonstration program

--- Task1B.py
+++ Task1B.py
@@ -5,10 +5,9 @@ from floodsystem.geo import stations_by_distance
 def run():
     """Requirements for Task 1B"""
 
-    # Build list of stations
-    stations = build_station_list()
+    stations = build_station_list() #  Builds list of stations
 
-    print(stations_by_distance(stations, [52.2053, 0.1218]))
+    #  implement demonstration program
 
 
 if __name__ == "__main__":
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,10 +13,8 @@ from .utils import sorted_by_key  # noqa
 
 def stations_by_distance(stations, p):
     distance_list = []  # create empty list
-    for s in stations:
+    for s in stations:  # iterate through list of stations
         distance = haversine(s.coord, p)  # calculate distance
         distance_tuple = (s, distance)  # create tuple
         distance_list.append(distance_tuple)  # add tuple to list
-    return sorted_by_key(distance_list, 1)
-
-    # order list by distance
+    return sorted_by_key(distance_list, 1)  # returns a list sorted by distance
