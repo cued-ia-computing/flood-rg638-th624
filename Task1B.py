ad80aabf0d45e307d817bba3fc190b144c23e88a
rg638 <rg638@cam.ac.uk>
2021-01-19 16:18:23 +0000

    Implemented function, now need to create demonstration program (Cleaned up code due to flake8 error)

--- Task1B.py
+++ Task1B.py
@@ -1,4 +1,5 @@
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
 
 
 def run():
@@ -6,7 +7,7 @@ def run():
 
     stations = build_station_list()  # Builds list of stations
 
-    #  implement demonstration program
+    stations_by_distance(stations, (52.2053, 0.1218))  #  implement demonstration program
 
 
 if __name__ == "__main__":
