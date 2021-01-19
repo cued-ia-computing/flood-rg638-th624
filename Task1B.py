6fef8c57e260003820717ebe5d6a5d768147e557
rg638 <rg638@cam.ac.uk>
2021-01-19 01:19:27 +0000

    Cleanup

--- Task1B.py
+++ Task1B.py
@@ -1,6 +1,7 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
+
 def run():
     """Requirements for Task 1B"""
 
@@ -8,6 +9,7 @@ def run():
     stations = build_station_list()
 
     print(stations_by_distance(stations, [52.2053, 0.1218]))
+
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IB Flood Warning System ***")
