3db08945cc0fe193a60427f37c064305ae7f7ca6
rg638 <rg638@cam.ac.uk>
2021-01-22 00:02:25 +0000

    Finished testing for 1B

--- test_geo.py
+++ test_geo.py
@@ -1,10 +1,19 @@
 """test for the geo module"""
 
-import floodsystem.stationdata
 import floodsystem.geo
+import floodsystem.station
+import floodsystem.stationdata
+
 
 def test_by_distance():
-    """Test stations by distance"""
+    """Test stations by distance function"""
 
-    stations = floodsystem.stationdata.build_stations_list  # build stations list
-    sorted_list = floodsystem.geo.stations_by_distance(stations, (52.2053, 0.1218))
+    stations = floodsystem.stationdata.build_station_list()
+    sorted_list = floodsystem.geo.stations_by_distance(stations, (0, 0))
+
+    # check tuple entries are of expected types
+    assert type(sorted_list[0][1]) == float
+    assert type(sorted_list[0][0]) == floodsystem.station.MonitoringStation
+    # check it is ordered correctly
+    assert sorted_list[0][1] <= sorted_list[3][1]
+    assert sorted_list[-1][1] >= sorted_list[0][1]
