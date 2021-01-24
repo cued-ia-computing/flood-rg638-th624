cf7555c0185b449cd5b3f526c5cb5234b98a5b04
rg638 <rg638@cam.ac.uk>
2021-01-24 22:34:12 +0000

    working on demonstration program

--- Task1C.py
+++ Task1C.py
@@ -1,11 +1,16 @@
-from floodsystem.geo import stations_by_distance
-from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
+
 
 def run():
     """Requirements for Task 1C"""
 
-
+    stations = stations_within_radius(build_station_list(), [52.2053, 0.1218], 10)
+    final_list = []
+    for s in stations:
+        name = s[0].station_id
+        final_list.append(name)
+    print(final_list.sort)
 
 
 if __name__ == "__main__":
