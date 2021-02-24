258fa7da383db8c6214c8460d53ed6e08e78a0b0
rg638 <rg638@cam.ac.uk>
2021-02-24 21:59:59 +0000

    demonstration programme done

--- Task2B.py
+++ Task2B.py
@@ -4,4 +4,7 @@ from floodsystem.flood import stations_level_over_threshold
 station_list = build_station_list()
 update_water_levels(station_list)
 
-print(stations_level_over_threshold(station_list, 0.8))
+list_of_tuples = stations_level_over_threshold(station_list, 0.8)
+
+for i in range(len(list_of_tuples)):  # cycle through tuples
+    print((list_of_tuples[i][0]).name, list_of_tuples[i][1])  # prints name and relative water level
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -2,7 +2,7 @@ from floodsystem.station import MonitoringStation
 
 
 def stations_level_over_threshold(stations, tol):
-    list_of_tuples = []
+    list_of_tuples = []  # create empty list
     for s in stations:
         if type(MonitoringStation.relative_water_level(s)) == float:
             if MonitoringStation.relative_water_level(s) > tol:
