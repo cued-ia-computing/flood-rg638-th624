753cf590661ce269084313bfb20af3ad8bacddde
rg638 <rg638@cam.ac.uk>
2021-02-24 22:20:48 +0000

    forgot to order list

--- Task2B.py
+++ Task2B.py
@@ -1,10 +1,18 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
-station_list = build_station_list()
-update_water_levels(station_list)
 
-list_of_tuples = stations_level_over_threshold(station_list, 0.8)
+def run():
+    """Requirements for task 2B"""
+    station_list = build_station_list()
+    update_water_levels(station_list)
 
-for i in range(len(list_of_tuples)):  # cycle through tuples
-    print((list_of_tuples[i][0]).name, list_of_tuples[i][1])  # prints name and relative water level
+    list_of_tuples = stations_level_over_threshold(station_list, 0.8)
+
+    for i in range(len(list_of_tuples)):  # cycle through tuples
+        print((list_of_tuples[i][0]).name, list_of_tuples[i][1])  # prints name and relative water level
+
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -2,10 +2,11 @@ from floodsystem.station import MonitoringStation
 
 
 def stations_level_over_threshold(stations, tol):
-    list_of_tuples = []  # create empty list
+    threshold_list = []  # create empty list
     for s in stations:
         if type(MonitoringStation.relative_water_level(s)) == float:
             if MonitoringStation.relative_water_level(s) > tol:
                 water_level_tuple = (s, MonitoringStation.relative_water_level(s))  # create tuple
-                list_of_tuples.append(water_level_tuple)  # append list with tuple if over threshold
-    return list_of_tuples
+                threshold_list.append(water_level_tuple)  # append list with tuple if over threshold
+    sorted_by_second = sorted(threshold_list, key=lambda tup: tup[1], reverse=True)
+    return sorted_by_second
