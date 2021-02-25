c200ecde3b27d2d4bcb9778b0648919b3108902e
rg638 <rg638@cam.ac.uk>
2021-02-25 17:52:36 +0000

    demonstration programme done

--- Task2C.py
+++ Task2C.py
@@ -1,12 +1,17 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
+import floodsystem.station
+
 
 def run():
 
     stations = build_station_list()
     update_water_levels(stations)
 
-    print(stations_highest_rel_level(stations, 10))
+    list_of_stations = stations_highest_rel_level(stations, 10)
+    for i in range(len(list_of_stations)):
+        print(list_of_stations[i].name, floodsystem.station.MonitoringStation.relative_water_level(list_of_stations[i]))
+
 
 if __name__ == "__main__":
     print("*** Task 1C: CUED Part IA Flood Warning System ***")
